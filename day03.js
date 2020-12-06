b625c6d66d7d4322b3af95443ce497f06ba876b2
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-06 21:49:48 +0200

    Update day03.js

--- day03.js
+++ day03.js
@@ -1,14 +1,24 @@
 //https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
 function trueOrFalse(val) {
-  if (val) return "true";         
-  else return "false";
+  if (val) {
+    return "true";
+  }
+  else { 
+    return "false";
+  }
 }
 __________________________________________________
 //https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
 function saleHotdogs(n) {
-  if (n < 5) return 100 * n;
-  else if ((n >= 5) & (n < 10)) return 95 * n;
-  else return 90 * n;
+  if (n < 5) { 
+    return 100 * n;
+  }
+  else if ((n >= 5) & (n < 10)) {
+    return 95 * n;
+  }
+  else {
+    return 90 * n;
+  }
 }
 ___________________________________________________
 //https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript
