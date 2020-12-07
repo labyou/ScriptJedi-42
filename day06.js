c34aa21a9e70c33c8e7cb44193aeb044c7a9ce7b
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-07 12:08:58 +0200

    Create day06.js

---
+++ day06.js
@@ -0,0 +1,22 @@
+//https://www.codewars.com/kata/5aba780a6a176b029800041c/train/javascript
+function maxMultiple(divisor, bound){
+  for (let n = bound; n <= bound; n--) {
+    if (n % divisor == 0) {
+      return n;
+    }
+  }
+}
+_______________________________________________________________
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+function hero(bullets, dragons){
+ if (bullets / 2 >= dragons) {
+   return true;
+ } else {
+   return false;
+ }
+}
+______________________________________________________________
+//https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+function unusualFive() {
+  return "Yura.".length;
+}
