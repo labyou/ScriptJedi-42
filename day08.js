d11759b1e774f10b4d10fac77d90de4fe6f86e06
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-08 10:09:23 +0200

    Update day08.js

--- day08.js
+++ day08.js
@@ -8,3 +8,19 @@ function whatNumberIsIt(n) {
   else if (n == Number.NEGATIVE_INFINITY) return "Input number is Number.NEGATIVE_INFINITY";
   else if (Number.isNaN(n)) return "Input number is Number.NaN";
 }
+__________________________________________________________________
+//https://www.codewars.com/kata/57238ceaef9008adc7000603/train/javascript
+function colorOf(r, g, b) { 
+  let red = r.toString(16);
+  if (red.length < 2) red = "0" + red;
+  let green = g.toString(16);
+  if (green.length < 2) green = "0" + green; 
+  let blue = b.toString(16);
+  if (blue.length < 2) blue = "0" + blue;
+  return `#${red + green + blue}`;
+}
+__________________________________________________________________
+//https://www.codewars.com/kata/57256064856584bc47000611/train/javascript
+function howManySmaller(arr,n) {
+  return arr.filter(number => Number(number.toFixed(2)) < n).length;
+}
