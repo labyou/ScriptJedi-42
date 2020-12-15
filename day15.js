0fd2e95253a04d69e30d174bd8444662734b679c
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-15 12:05:34 +0200

    Update day15.js

--- day15.js
+++ day15.js
@@ -9,3 +9,20 @@ let shuffleIt = (arr, ...arrsOfIndex) => {
   }
   return array;
 }
+//https://www.codewars.com/kata/572af273a3af3836660014a1/train/javascript
+let infiniteLoop = (arr, d, n) => {
+  const array = arr;
+  for (let i = 1; i <= n; i++) {
+    if (d === 'left') {
+      arr[2].push(arr[0].shift());
+      arr[1].push(arr[2].shift());
+      arr[0].push(arr[1].shift());
+    } else if (d === 'right') {
+      arr[0].unshift(arr[2].pop());
+      arr[1].unshift(arr[0].pop());
+      arr[2].unshift(arr[1].pop());
+    }
+  }
+  return array;
+}
+//https://www.codewars.com/kata/572cb264362806af46000793/train/javascript
