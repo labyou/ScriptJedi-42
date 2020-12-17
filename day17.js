db136cc4ed5648de1e4435fab34d7748313b5543
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-17 18:13:43 +0200

    Create day17.js

---
+++ day17.js
@@ -0,0 +1,24 @@
+//https://www.codewars.com/kata/5731861d05d14d6f50000626/train/javascript
+const bigToSmall = (arr) => {
+  let array = [].concat(...arr);
+  array = array.sort((a, b) => b - a);
+  return array.join('>');
+}
+//https://www.codewars.com/kata/573156709a231dcec9000ee8/train/javascript
+const tailAndHead = (arr) => {
+  let array = [];
+  for(let i = 0; i < arr.length - 1; i++) {
+    let lastNum = arr[i] % 10;
+    let firstNum = Number(String(arr[i + 1])[0]);
+    array = [...array, lastNum + firstNum];
+  }
+  return array.reduce((a, b) => a * b); 
+}
+//https://www.codewars.com/kata/5732b0351eb838d03300101d/train/javascript
+const blackAndWhite = (arr) => {
+  if (Array.isArray(arr)) {
+    if (arr.indexOf(5) >= 0 && arr.indexOf(13) >= 0) return "It's a black array";
+    return "It's a white array";
+  } else return "It's a fake array"; 
+}
+//
