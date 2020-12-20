dc1f75e63233b26ab3069dd6c69e387bcf370e1e
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-20 17:32:40 +0200

    Update day21.js

--- day21.js
+++ day21.js
@@ -1,3 +1,5 @@
 //https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
 let regex = /(http:\/\/|https:\/\/)[a-z0-9\.]+(\.com|\.net)/ig;
+// http://www.codewars.com/kata/573e6831e3201f6a9b000971
+let regex = /\b(\w)(\w?)(\w?).?\3\2\1\b/g
 //
