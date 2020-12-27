dec857c6cdfaa633300ae4a7b01b81e358daf8ac
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-27 23:29:14 +0200

    Update day26.js

--- day26.js
+++ day26.js
@@ -1,1 +1,7 @@
+//http://www.codewars.com/kata/power-bind
 
+
+//https://www.codewars.com/kata/559f3e20f4f29869cf0000ea/train/javascript
+const OrderPeople = people => {
+  return people.sort( (a,b) => a.age - b.age ); //complete this function
+}
