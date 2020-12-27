8cb9928cff5c18f10eb0c196a66c6bcaa18f3293
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-28 00:18:49 +0200

    Update day26.js

--- day26.js
+++ day26.js
@@ -1,6 +1,11 @@
 //http://www.codewars.com/kata/power-bind
-
-
+Function.prototype.bind = function(ctx){
+  let func = this;
+  return function(...args){
+    if(this == global) return func.apply(ctx, args);
+    else return func.apply(this, args);
+  };
+};
 //https://www.codewars.com/kata/559f3e20f4f29869cf0000ea/train/javascript
 const OrderPeople = people => {
   return people.sort( (a,b) => a.age - b.age ); //complete this function
