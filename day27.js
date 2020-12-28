25466a7a031413398a369b8c666c1990bc8233bf
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2020-12-28 16:35:41 +0200

    Update day27.js

--- day27.js
+++ day27.js
@@ -1,1 +1,19 @@
-//
+//https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+const buildFun = (n) => {
+	const res = [];
+	for (let i = 0; i < n; i++){
+		res.push(function() {
+			return i;
+		});
+	}
+	return res;
+}
+//https://www.codewars.com/kata/shifty-closures/train/javascript
+let greet_abe = function() {
+  let name = 'Abe';
+  return "Hello, " + name + '!';
+};
+const greet_ben = function() {
+  let name = 'Ben';
+  return "Hello, " + name + '!';
+};
