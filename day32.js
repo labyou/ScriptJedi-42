affbf05b8b9361105e8095235fbf1022ca42f6bc
YuraLabetskiy <53494036+YuraLabetskiy@users.noreply.github.com>
2021-01-08 14:07:02 +0200

    Create day32.js

---
+++ day32.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/547c71fdc5b2b38db1000098/train/javascript
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = this.firstName + ' ' + this.lastName
+}
+//https://www.codewars.com/kata/547f1a8d4a437abdf800055c/train/javascript
+class NamedOne {
+  constructor(first, last) 
+  { 
+    this.firstName = first; this.lastName = last 
+  }
+  get fullName() { 
+    return this.firstName + ' ' + this.lastName 
+  }
+  set fullName(variable) 
+  { 
+    if (/ /.test(variable)) [this.firstName, this.lastName] = variable.split(' ') 
+  }
+}
